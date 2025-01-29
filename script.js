77009e3330872805f3e606860e63091c5a20a37e
DevTechG <gabriel10300007@gmail.com>
2025-01-29 16:25:35 -0300

    Capturing the submit event from the form.

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
+// Obtendo os elementos do formulário.
 const amount = document.getElementById("amount")
+const currency = document.getElementById("currency")
+const form = document.querySelector("form")
 
 // Manipulando o input amount para receber somente números.
 amount.addEventListener("input", () => {
     
     const hasCharactersRegex = /\D+/g
     amount.value = amount.value.replace(hasCharactersRegex, "")
-    
+
 })
+
+// Capturando o evento de submit (enviar) do formulário.
+form.onsubmit = (event) => {
+    event.preventDefault()
+
+    
+}
