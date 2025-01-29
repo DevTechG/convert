e1d7152efa4a87b743e6b65ffb282c146e844fc5
DevTechG <gabriel10300007@gmail.com>
2025-01-29 16:04:42 -0300

    Creating the function that limits you to receiving only numbers.

--- script.js
+++ script.js
@@ -2,5 +2,8 @@ const amount = document.getElementById("amount")
 
 // Manipulando o input amount para receber somente números.
 amount.addEventListener("input", () => {
-
+    
+    const hasCharactersRegex = /\D+/g
+    amount.value = amount.value.replace(hasCharactersRegex, "")
+    
 })
